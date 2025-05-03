3c6c77ce55a469750e03c0eb1755184316a9720f
TheM4F <mstafa.gnc2@gmail.com>
2025-05-03 20:51:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const siteConfig = {
   
     // Embed iframe için src URL’si
     mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.2846694357794!2d33.25429890256442!3d39.90660088890532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4081fd8000611ce7%3A0x3b59cdbac16637ae!2zR2Vuw6cgw4dpZnRsacSfaQ!5e0!3m2!1str!2str!4v1746204930947!5m2!1str!2str",
-    discordWebhookUrl: "https://canary.discord.com/api/webhooks/1368181396555825222/Hw-DHdozBCTssFmhAOPrbvYP7aSc97dwBeyjTbRvp1eX8w1mlitXT80-oIjFDc-RQf3C", 
+    discordWebhookUrl: "https://canary.discord.com/api/webhooks/1368283825792094238/WnzUOoxNNtF7nwmt5no63y-pgbUyEFnUr6-8tnS0HyBP5HdKB4F0Yt0LFtdfboCg-Ot5", 
     categories: [
       { title: "Kurban Kesim Hizmeti", description: "İslami usullere uygun, hijyenik ve titiz kesim süreçleri.", bgImage: "assets/kurban-kesim.webp" },
       { title: "Tarla Ekim & Sürüm",     description: "Modern ekipmanlarla arazi hazırlığı, ekim ve sürüm işlemleri.",            bgImage: "assets/tarla-ekim.webp" },
