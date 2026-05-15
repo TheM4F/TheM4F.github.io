8f7df59b80129b4cbf8358d96a59f0f9418b0ff8
TheM4F <52375646+TheM4F@users.noreply.github.com>
2026-05-15 15:03:19 +0300

    x

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-  corePlugins: {
-    preflight: false, // Reset’i kapat: kendi CSS’in bozulmasın
-  },
-};
